659d1ee838ff3a2df46e9c59d70981fec70a05d4
P0v0n <pavandange@everymedia.world>
2025-10-28 18:11:48 +0530

    chore(ui): remove global search bar from DashboardLayout header

--- src/components/layout/DashboardLayout.jsx
+++ src/components/layout/DashboardLayout.jsx
@@ -7,7 +7,6 @@ import {
   LayoutDashboard,
   BarChart3,
   Settings,
-  Search,
   Bell,
   User,
   Menu,
@@ -176,16 +175,6 @@ export default function DashboardLayout({ children }) {
               >
                 <Menu className="w-6 h-6" />
               </button>
-
-              {/* Search */}
-              <div className="relative">
-                <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400" />
-                <input
-                  type="text"
-                  placeholder="Search..."
-                  className="pl-10 pr-4 py-2 w-64 lg:w-96 border border-gray-300 dark:border-gray-600 rounded-lg bg-gray-50 dark:bg-gray-700 text-gray-900 dark:text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-              </div>
             </div>
 
             <div className="flex items-center gap-4">
